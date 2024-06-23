c691b3a532996280b0bde4164ffccb038052f938
Daniel-C-Martins <danicoxa.martins@outlook.com>
2024-06-22 22:07:11 -0300

    algumas melhorrias esteticas

--- code/app.py
+++ code/app.py
@@ -5,21 +5,23 @@ from digraph import Digraph
 from time import process_time
 
 list_boxes = [] #Listas de caixas
-longest_paths = {}
-paths = []
+longest_paths = {} #Dicionario dos maiores caminhos de cada vertice("Folha") até outro vertice
+paths = [] 
 
+# Função responsável por ler as caixas do txt
 def read_boxes(chosen):
     global list_boxes
     #Leitura dos dados
     with open(f"Casos\\{chosen}.txt", "r") as archive: #Leitura das linhas do arquivo txt para uma variável 
         for lines in archive:         #Itera sobre as linhas do txt
-            measure = list(map(int, lines.strip().split()))  #Troca o valor das dimensoes para int
-            measure.sort()                              #Ordena para comparação
-            smallest, medium, greater = measure  #Adiciona os valores em ordem
-            name = str(greater) + str(medium) + str(smallest) 
-            box = Box(name, greater, medium, smallest)      #Cria a caixa com o nome e seus tamanhos
+            measure = list(map(int, lines.strip().split()))  #Converte os 3 valores da linha para uma lista de int
+            measure.sort()                              #Ordena a lista para comparação
+            smallest, medium, bigger = measure  #Adiciona os valores em ordem, esses valores serão usados paa criar a caixa
+            name = str(bigger) + str(medium) + str(smallest) #Criando o nome da caixa
+            box = Box(name, bigger, medium, smallest)      #Cria a caixa com o nome e seus tamanhos
             list_boxes.append(box)      #Coloca a caixa na lista
-        
+
+#Função responsável por comparar os tamanhos das caixas      
 def compare_boxes():
     global list_boxes
 
@@ -27,20 +29,22 @@ def compare_boxes():
         for j in range(len(list_boxes)):    # Fixa uma caixa e compara com as outras
             if i == j:          #Se a comparação for da caixa com ela mesma, pula a comparação
                 continue
-            elif (list_boxes[i].get_large_side() < list_boxes[j].get_large_side() and
+            elif (list_boxes[i].get_bigger_side() < list_boxes[j].get_bigger_side() and
                 list_boxes[i].get_medium_side() < list_boxes[j].get_medium_side() and   #Comparando se as 3 dimensoes da caixa cabem na outra
                 list_boxes[i].get_small_side() < list_boxes[j].get_small_side()):
-                list_boxes[i].add_contida(list_boxes[j].get_name())      #Adiciona em uma lista dentro de cada caixa, que ela cabe dentro da outra caixa
-                list_boxes[j].add_contem(list_boxes[i].get_name())      #Adiciona em um lista dentro de cada caixa, que esta caixa contem outra caixa
+                list_boxes[i].add_contained(list_boxes[j].get_name())      #Adiciona em uma lista dentro de cada caixa, que ela cabe dentro da outra caixa
+                list_boxes[j].add_contain(list_boxes[i].get_name())      #Adiciona em um lista dentro de cada caixa, que esta caixa contem outra caixa
 
+#Função usada para escrever um texto com as ligações
 def write_txt():
     global list_boxes
     with open("Resultados\\grafo.txt", "w") as archive:     #Abrindo o arquivo de saida
         for box in list_boxes:          #Iterando sobre toda a lista
-            for box_id in box.esta_contida:    #Pegando da caixa as caixas maiores que ela
+            for box_id in box.contained:    #Pegando da caixa as caixas maiores que ela
                 line = f"{box.get_name()} {box_id} \n"   #Criando a linha para escrevendo no txt
                 archive.write(line)         #Escrevendo no txt
 
+#Função usada para gera o .dot 
 def dot(d):
     global list_boxes
     with open("Resultados\\grafo.dot", "w") as archive:
@@ -51,10 +55,10 @@ def longest_path(d):
     # Inicialize uma vez para todos os vértices
     
     for box in list_boxes:
-        if box.contem_IsEmpty():
-            lp = LongestPathDAG(d, box.get_name())
-            lp.find_longest_path()
-            longest_paths[box.get_name()] = lp
+        if box.contain_IsEmpty():               #Pega todos vertices com grau de entrada igual a 0
+            lp = LongestPathDAG(d, box.get_name())  
+            lp.find_longest_path()      #Calcula o maior caminho de cada vertice
+            longest_paths[box.get_name()] = lp  #Adiciona no dicionario o caminho
 
 def calc_longest_path():
     global list_boxes, longest_paths, paths
@@ -68,24 +72,24 @@ def calc_longest_path():
                     path_length = len(path)
                     paths.append((source_box.get_name(), target_box.get_name(), path, path_length))
 
+#Função usada para descobrir e printar o maior caminho
 def print_longest_path():
     global paths
-    # Encontre o maior comprimento
-    max_length = 0
-    max_path_info = None
+   
+    max_length = 0          #Variavel usada para guardar o comprimento do maior caminho
+    max_path_info = None    #Variavel usada para guardar as infos sobre o maior caminho
     for source, target, path, length in paths:
-        if length > max_length:
+        if length > max_length: 
             max_length = length
             max_path_info = (source, target, path)
 
-    # Imprima o maior comprimento encontrado
-    if max_path_info is not None:
+    
+    if max_path_info is not None:       #Print do maior caminho encontrado
         source, target, path = max_path_info
         print(f"O maior comprimento de todos os caminhos é: {max_length}")
         print(f"O caminho mais longo correspondente é de {source} para {target}: {path}")
-    else:
-        print("Não há caminhos válidos no grafo.")
 
+#Função usada para criar o menu de escolha de entrada
 def menu():
     option = input("""
     ===============================          
@@ -129,7 +133,7 @@ def menu():
     else:
         menu()
             
-
+#Função principal da aplicação
 def main():
     global list_boxes
 
@@ -146,51 +150,6 @@ def main():
     final_time = process_time()
     print(f"O tempo para rodar o {option} foi: {final_time - start_time}")
 
-
-
-
-
-
-    # # Inicialize uma vez para todos os vértices
-    # longest_paths = {}
-    # for box in list_boxes:
-    #     if box.contem_IsEmpty():
-    #         source_name = box.get_name()
-    #         lp = LongestPathDAG(d, source_name)
-    #         lp.find_longest_path()
-    #         longest_paths[source_name] = lp
-
-    # paths = []
-
-    # # Agora calcule os caminhos para todos os pares
-    # for source_box in list_boxes:
-    #     for target_box in list_boxes:
-    #         source_name = source_box.get_name()
-    #         target_name = target_box.get_name()
-    #         if source_name in longest_paths:
-    #             path = longest_paths[source_name].path_to(target_name)
-    #             if path is not None:  # Adiciona apenas se o caminho for válido (não vazio)
-    #                 path_length = len(path)
-    #                 paths.append((source_name, target_name, path, path_length))
-
-    # # paths agora contém todos os caminhos de todos os vértices para todos os outros vértices
-    # # e os comprimentos dos caminhos
-
-    # # Encontre o maior comprimento
-    # max_length = 0
-    # max_path_info = None
-    # for source, target, path, length in paths:
-    #     if length > max_length:
-    #         max_length = length
-    #         max_path_info = (source, target, path)
-
-    # # Imprima o maior comprimento encontrado
-    # if max_path_info is not None:
-    #     source, target, path = max_path_info
-    #     print(f"O maior comprimento de todos os caminhos é: {max_length}")
-    #     print(f"O caminho mais longo correspondente é de {source} para {target}: {path}")
-    # else:
-    #     print("Não há caminhos válidos no grafo.")
 
 if __name__ == "__main__":
     main() 
--- code/box.py
+++ code/box.py
@@ -1,24 +1,24 @@
 class Box:
 
-    def __init__(self, name, large_side, medium_side, small_side):   #Construtor da caixa
+    def __init__(self, name, bigger_side, medium_side, small_side):   #Construtor da caixa
         self._name = name
-        self._large_side = large_side
+        self._bigger_side = bigger_side
         self._medium_side = medium_side
         self._small_side = small_side
-        self.contem = []   #Lista de caixas que cabem dentro dessa caixa
-        self.esta_contida = [] #Lista de caixas que esta caixa cabe dentro 
+        self.contain = []   #Lista de caixas que cabem dentro dessa caixa
+        self.contained = [] #Lista de caixas que esta caixa cabe dentro 
       
 
     def __str__(self):
-        contem_str = ', '.join(str(item) for item in self.contem)
-        return f"Id da caixa: {self._name}, Maior lado: {self._large_side}, Lado médio: {self._medium_side}, menor lado: {self._small_side}, contém: {contem_str}"
+        contain_str = ', '.join(str(item) for item in self.contain)
+        return f"Id da caixa: {self._name}, Maior lado: {self._bigger_side}, Lado médio: {self._medium_side}, menor lado: {self._small_side}, contém: {contain_str}"
 
 
     def get_name(self):
         return self._name
     
-    def get_large_side(self):
-        return self._large_side
+    def get_bigger_side(self):
+        return self._bigger_side
 
     def get_medium_side(self):
         return self._medium_side
@@ -26,21 +26,21 @@ class Box:
     def get_small_side(self):
         return self._small_side
     
-    def get_contem(self):
-        contem_str = ', '.join(str(item) for item in self.contem)
-        return contem_str
+    def get_contain(self):
+        contain_str = ', '.join(str(item) for item in self.contain)
+        return contain_str
     
     def get_contida(self):
-        contem_str = ', '.join(str(item) for item in self.esta_contida)
-        return contem_str
+        contain_str = ', '.join(str(item) for item in self.contained)
+        return contain_str
     
-    def add_contem(self, num):
-        self.contem.append(num)
+    def add_contain(self, num):
+        self.contain.append(num)
 
-    def add_contida(self, num):
-        self.esta_contida.append(num)
+    def add_contained(self, num):
+        self.contained.append(num)
 
-    def contem_IsEmpty(self):
-        return not self.contem
+    def contain_IsEmpty(self):
+        return not self.contain
 
 
